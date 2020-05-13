d37f4fdea544ff7ce596d8dc8ef94a4f2ee0451e
Impulse Impulse <smetiyk@gmail.com>
2020-05-13 19:54:52 +0300

    implemented incorrect logic for the first demo

--- src/pages/MainPage/MPParagraphs/Paragraphs.js
+++ src/pages/MainPage/MPParagraphs/Paragraphs.js
@@ -2,17 +2,20 @@
 import React, { useState, useEffect } from 'react'
 import './index.styl'
 import { Skeleton } from 'antd'
-import test from './Порядок_РК_ЕЭК_161_06122016.pdf'
+// import test from './Порядок_РК_ЕЭК_161_06122016.pdf'
 import mammoth from 'mammoth'
+import __html from './Порядок_РК_ЕЭК_161_06122016.js'
+var template = { __html };
 
 const Paragraphs = (props) => {
-  const te111 = require('./Порядок_РК_ЕЭК_161_06122016.pdf')
-  const te112 = require('./Порядок_Минпромторг_1328_20082013.pdf')
-  const te113 = require('./Адм.регламент_Росстандарт_2173_20092019.pdf')
-
+  // const te112 = require('./Порядок_Минпромторг_1328_20082013.pdf')
+  // const te113 = require('./Адм.регламент_Росстандарт_2173_20092019.pdf')
+  /*  */
+  debugger
   return (
     <div className='Paragraphs-root'>
-      {
+      <div dangerouslySetInnerHTML={template} />
+      {/* {
         props.currentSelectedId ?
           (props.currentSelectedId === 1 || props.currentSelectedId === 3) ?
             <iframe src={te111} style={{ width: '100%', height: '100%' }} />
@@ -20,7 +23,7 @@ const Paragraphs = (props) => {
               <iframe src={te112} style={{ width: '100%', height: '100%' }} />
               : <iframe src={te113} style={{ width: '100%', height: '100%' }} />
           : <Skeleton active />
-      }
+      } */}
     </div>
   )
 }

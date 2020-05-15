b4ace32e94a46b2d2ad8336d0ec80e339efe5c82
Impulse Impulse <smetiyk@gmail.com>
2020-05-15 17:35:46 +0300

    Updates

--- precache-manifest.2ec73d504446b954d15ba17892422432.js
+++ precache-manifest.2ec73d504446b954d15ba17892422432.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3e87134a5166fdf4de246f369d5c56c5",
+    "revision": "c450853ccc6c999734b823d6210c95e0",
     "url": "/Table-project/index.html"
   },
   {
-    "revision": "c05314ea2b357c652642",
+    "revision": "65dfdcf79615758234da",
     "url": "/Table-project/static/css/2.6afbe6a5.chunk.css"
   },
   {
-    "revision": "c05314ea2b357c652642",
-    "url": "/Table-project/static/js/2.4310f145.chunk.js"
+    "revision": "65dfdcf79615758234da",
+    "url": "/Table-project/static/js/2.ac1397c8.chunk.js"
   },
   {
     "revision": "f576af3254df0679363d3e6ffe804e36",
-    "url": "/Table-project/static/js/2.4310f145.chunk.js.LICENSE.txt"
+    "url": "/Table-project/static/js/2.ac1397c8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a57426d3773e66e7c7e7",
-    "url": "/Table-project/static/js/main.94f605b5.chunk.js"
+    "revision": "46e92cab777a78b37c18",
+    "url": "/Table-project/static/js/main.c00b9b27.chunk.js"
   },
   {
     "revision": "c8715ae28e0f75ebfdde",

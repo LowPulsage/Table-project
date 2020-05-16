74d3ad331c1e7bce22bbe2ea4bf8dcb29f796147
Impulsation <smetiyk@gmail.com>
2020-05-16 13:16:23 +0300

    upd

--- src/pages/MainPage/MPParagraphs/Paragraphs.js
+++ src/pages/MainPage/MPParagraphs/Paragraphs.js
@@ -1,30 +1,14 @@
-/*eslint-disable*/
 import { setFragmentForSearching } from '../../../modules/session/session-reducers'
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import formatDoc from './formatDoc'
 import './index.styl'
 
-// import fz_102 from './Documents/ФЗ_102_26062008.js'
-// import prot_10 from './Documents/Протокол_10_ЕЭС_29052014.js'
-// import por_pc_98 from './Documents/Порядок_РС_ЕЭК_98_18102016.js'
-// import por_pc_21 from './Documents/Порядок_РС_ЕЭЕК_21_17032016.js'
-// import por_pk_89 from './Documents/Порядок_РК_ЕЭК_89_26072016.js'
-// import por_pk_68 from './Documents/Порядок_РК_ЕЭК_68_070062016.js'
-// import por_prov from './Documents/Порядок_проведения_МЭП_РК_ЕЭК_10_24012017.js'
-// import por_min_4091 from './Documents/Порядок_Минпромторг_России_4091_15122015.js'
-// import por_min_2167 from './Documents/Порядок_Минпромторг_России_2167_30072015.js'
-// import por_min_13281 from './Documents/Порядок_Минпромторг_1328_20082013.js'
-// import por_min_1081 from './Documents/Порядок_Минпромторг_1081_30112009.js'
-// import pol_879 from './Documents/Положение_879_31102009.js'
-// import per_24 from './Documents/Перечень_РК_ЕЭК_24_21042015.js'
-// import adm_2173 from './Documents/Адм.регламент_Росстандарт_2173_20092019.js'
-// import excelDoc from './ExcelDocumentList/Близкие-фрагменты-разный-размер.js
-
-const Paragraphs = (props) => {
+const Paragraphs = () => {
   const [htmlObj, setHtml] = useState({ __html: '' })
   const dipatch = useDispatch()
   const selectedWordFileName = useSelector(state => state.source.selectedWordFileName)
+  const fragmentForSearching = useSelector(state => state.source.fragmentForSearching)
   const allDocsFragments = useSelector(state => state.source.allDocsFragments)
 
   useEffect(() => {
@@ -33,11 +17,26 @@ const Paragraphs = (props) => {
     setHtml({ __html })
   }, [])
 
+  const selectFragment = e => {
+    if (e.target.id) {
+      dipatch(setFragmentForSearching(e.target.id))
+
+      if (fragmentForSearching) {
+        const oldId = fragmentForSearching.replace(/ /g, '_')
+        const node = document.getElementById(oldId)
+        if (node) node.classList.remove('active-fragment')
+      }
+
+      const node = document.getElementById(e.target.id)
+      if (node) node.classList.add('active-fragment')
+    }
+  }
+
   return (
-    <div 
-      onClick={e => { dipatch(setFragmentForSearching(e.target.id)) }}
+    <div
       dangerouslySetInnerHTML={htmlObj}
       className='Paragraphs-root'
+      onClick={selectFragment}
     />
   )
 }

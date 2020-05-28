3fc39fcdb295a89ca8908e036d3c35cdb23fc574
Impulsation <goddyimi@gmail.com>
2020-05-28 19:22:13 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Table-project/precache-manifest.6a26a9b1c5387f208587955363473116.js"
+  "/Table-project/precache-manifest.00b0e41724f2cd896ead5d0dea27ecb7.js"
 );
 
 self.addEventListener('message', (event) => {

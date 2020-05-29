c6739eb9925851e5c90b037f4cd774aa1cb73bbe
Impulse Impulse <smetiyk@gmail.com>
2020-05-29 19:14:50 +0300

    Updates

---
+++ precache-manifest.dca4bcdcaffd1a979a8d880ddff9bc12.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a1cfcaeadb8243e9ce8c32eea0a5d5",
+    "url": "/Table-project/index.html"
+  },
+  {
+    "revision": "d8e629eab6d5ef8609e0",
+    "url": "/Table-project/static/css/2.c936730f.chunk.css"
+  },
+  {
+    "revision": "d8e629eab6d5ef8609e0",
+    "url": "/Table-project/static/js/2.e511865f.chunk.js"
+  },
+  {
+    "revision": "f576af3254df0679363d3e6ffe804e36",
+    "url": "/Table-project/static/js/2.e511865f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02af136b34fcfe52f08f",
+    "url": "/Table-project/static/js/main.474eac5f.chunk.js"
+  },
+  {
+    "revision": "90c2983c3158a9ec1063",
+    "url": "/Table-project/static/js/runtime-main.3d54572b.js"
+  },
+  {
+    "revision": "682a5c3e893a9ef91d487e589359d032",
+    "url": "/Table-project/static/media/exel.682a5c3e.png"
+  },
+  {
+    "revision": "fd28d30d4d8b3b1d1fe77c27866b3696",
+    "url": "/Table-project/static/media/folder.fd28d30d.png"
+  },
+  {
+    "revision": "ac79e652f9e03ac6908f64eb25adeebe",
+    "url": "/Table-project/static/media/hotpng.com.ac79e652.png"
+  }
+]);
